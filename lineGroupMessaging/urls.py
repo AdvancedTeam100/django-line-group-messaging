dfa00f151238e3e47d9156db0965809fee77a3af
setpis <superdev43@gmail.com>
2023-07-26 19:18:48 +0200

    fix favicon url

--- lineGroupMessaging/urls.py
+++ lineGroupMessaging/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Manager.urls')),
+    path('favicon.ico',RedirectView.as_view(url=staticfiles_storage.url('favicon.ico')))
 ]
